dc4f7c38e878501e3cc343b9b2bc465bf62bad7f
oh-its-ai <65685016+oh-its-ai@users.noreply.github.com>
2024-04-05 12:33:06 +0200

    added graph stuff

---
+++ Assets/Graph/Scripts/Window_Graph.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Window_Graph : MonoBehaviour {
+
+    [SerializeField] private Sprite circleSprite;
+    private RectTransform graphContainer;
+
+    private void Awake() {
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+
+        List<int> valueList = new List<int>() {  };
+        ShowGraph(valueList);
+    }
+
+    private GameObject CreateCircle(Vector2 anchoredPosition) {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        return gameObject;
+    }
+
+    public void ShowGraph(List<int> valueList) {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 100f;
+        float xSize = 20f;
+
+        GameObject lastCircleGameObject = null;
+        for (int i = 0; i < valueList.Count; i++) {
+            float xPosition = xSize + i * xSize;
+            float yPosition = (valueList[i] / yMaximum) * graphHeight;
+            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+            if (lastCircleGameObject != null) {
+                CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+            }
+            lastCircleGameObject = circleGameObject;
+        }
+    }
+
+    private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB) {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1,1,1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, GetAngleFromVector2D(dir));
+    }
+
+    public static float GetAngleFromVector2D(Vector2 vector)
+    {
+        vector = vector.normalized; 
+        float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg; 
+        if (angle < 0) angle += 360; 
+        return angle;
+    }
+}
--- Assets/SimulationController.cs
+++ Assets/SimulationController.cs
@@ -20,8 +20,12 @@ public class SimulationController : MonoBehaviour
     public Vector3 windSpeed;
     public static SimulationController Instance { get; private set; }
 
+    public int GraphScalar = 80;
+    public int GraphOffset = -10;
+
     // GameObjects
     public TextMesh ProtocolText;
+    public Window_Graph WindowGraph;
     
     // CUBE 1 Stuff
     public CubeController Cube1;
@@ -30,8 +34,10 @@ public class SimulationController : MonoBehaviour
     private Vector3 Cube1_Vel;
     
     // Data
+    private int _lastLoggedSecond = 0;
     private float _msSinceStart = 0f;
     private float _secondsSinceStart = 0;
+    public List<int> valueList = new List<int>() {};
     private void Awake() 
     { 
         // If there is an instance, and it's not me, delete myself.
@@ -58,6 +64,16 @@ public class SimulationController : MonoBehaviour
         _secondsSinceStart = _msSinceStart % 60;
         Cube1_Vel = Cube1.GetVel();
         TextMesh1.text = Cube1_Vel.ToString();
+
+        int secondsSinceStartInt = (int)_secondsSinceStart;
+        if (secondsSinceStartInt > _lastLoggedSecond)
+        {
+            _lastLoggedSecond = (int)_secondsSinceStart;
+            int newValue = ((int)(GraphScalar * Cube1_Vel.x)) + GraphOffset;
+            valueList.Add(newValue);
+            UpdateGraphs();
+        }
+        
     }
 
     void OnApplicationQuit() {
@@ -75,6 +91,11 @@ public class SimulationController : MonoBehaviour
         String temp = ProtocolText.text;
         String newText = "\n" + $"{_secondsSinceStart:0.00}" + ": " + text ;
         ProtocolText.text = temp + newText;
+    }
+
+    private void UpdateGraphs()
+    {
+        WindowGraph.ShowGraph(valueList);
     }
     
 }
