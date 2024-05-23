d92ff585e55f14a78644d66df1e7a5ec103f0671
oh-its-ai <65685016+oh-its-ai@users.noreply.github.com>
2024-05-23 18:09:55 +0200

    cleanup

--- Assets/Scripts/Lab/LabStatePhase5.cs
+++ Assets/Scripts/Lab/LabStatePhase5.cs
@@ -12,80 +12,65 @@ namespace Lab
     [CreateAssetMenu(fileName = "Phase5_", menuName = "Phasen/Phase5", order = 1)]
     public class LabStatePhase5 : LabStatePhase4
     {
-        private CubeLController _cubeL;
-        private CubeController _cube2;
-        
-        private float _traegheitsmomentCube2;
-        private float _traegheitsmomentCubeL;
-        private float _angleSpeed = 0;
-
-        
-        
         private Rigidbody _rbCubeLCube1;
         private Rigidbody _rbCubeLCube2;
         private Rigidbody _rbCubeLCube3;
         private Vector3 _cube2HitImpuls;
 
-        private Vector3 drehImpuls;
+        private Vector3 _drehImpuls;
         
         private List<LBodyPart> _lBodyParts = new List<LBodyPart>();
         private Vector3 _cube2HitDir;
 
         public override void OnStateEnter()
         {
-            
             Sim.SetWorldSpeed(stateWorldSpeed);
             Sim.SetUpdateGraphs(false);
-            _cubeL = Sim.cubeL;
-            _cube2 = Sim.cube2;
             
-            _rbCubeLCube1 = _cubeL.cube1.GetComponent<Rigidbody>();
-            _rbCubeLCube2 = _cubeL.cube2.GetComponent<Rigidbody>();
-            _rbCubeLCube3 = _cubeL.cube3.GetComponent<Rigidbody>();
+            _rbCubeLCube1 = CubeL.cube1.GetComponent<Rigidbody>();
+            _rbCubeLCube2 = CubeL.cube2.GetComponent<Rigidbody>();
+            _rbCubeLCube3 = CubeL.cube3.GetComponent<Rigidbody>();
             
             _lBodyParts.Add(new LBodyPart(_rbCubeLCube1, CubePos.Mitte));
             _lBodyParts.Add(new LBodyPart(_rbCubeLCube2, CubePos.Aussen));
             _lBodyParts.Add(new LBodyPart(_rbCubeLCube3, CubePos.Mitte));
-            _lBodyParts.Add(new LBodyPart(_cube2.GetRidgidBody(), CubePos.Aussen));
-
-            _cube2HitImpuls = _cube2.GetMaxSpeed() * _cube2.GetMass();
-            _cube2HitDir = _cube2.GetMaxSpeed().normalized;
+            _lBodyParts.Add(new LBodyPart(Cube2.GetRidgidBody(), CubePos.Aussen));
             
             // register hit
-            _cube2.AddToJoint(JointCubeL);
-            _cube2.DisableCollider();
+            Cube2.AddToJoint(JointCubeL);
+            Cube2.DisableCollider();
         }
 
         public override void StateUpdate()
         {
             float sumTgm = CalcSumTraegheitsMoment(_lBodyParts);
-            drehImpuls = (sumTgm) * Cube2.GetRidgidBody().angularVelocity;
+            _drehImpuls = (sumTgm) * Cube2.GetRidgidBody().angularVelocity;
             
             Debug.Log("Bahndrehimpuls: " + BahnDrehImpuls + " : Mag: " + BahnDrehImpuls.magnitude);
-            Debug.Log("L EigenDrehimpuls: V3: " + drehImpuls + " : Mag: " + drehImpuls.magnitude);
+            Debug.Log("L EigenDrehimpuls: V3: " + _drehImpuls + " : Mag: " + _drehImpuls.magnitude);
         }
         
         public override void LogUpdate()
         {
             base.LogUpdate();
-            GetLogValues().AddValue("L_EigenDrehImpuls", drehImpuls.magnitude);
-            GetLogValues().AddValue("L_GesamtDrehImpuls", BahnDrehImpuls.magnitude + drehImpuls.magnitude);
+            GetLogValues().AddValue("L_EigenDrehImpuls", _drehImpuls.magnitude);
+            GetLogValues().AddValue("L_GesamtDrehImpuls", BahnDrehImpuls.magnitude + _drehImpuls.magnitude);
         }
         
         public float CalcAngleSpeed()
         {
-            float I_cubeL = _cubeL.GetTraegheitsmoment() + _cubeL.GetMass()* _cubeL.GetDistanceCubeToAxis();
+            float I_cubeL = CubeL.GetTraegheitsmoment() + CubeL.GetMass()* CubeL.GetDistanceCubeToAxis();
             Debug.Log("I_cubeL: " + I_cubeL);
-            float cube2distance2Axis = Vector3.Distance(_cube2.transform.position, _cubeL.fixedJoint.transform.position);
+            float cube2distance2Axis = Vector3.Distance(Cube2.transform.position, CubeL.fixedJoint.transform.position);
             Debug.Log("cube2distance2Axis: " + cube2distance2Axis);
-            float I_cube2 = _cube2.GetTraegheitsmoment() + _cube2.GetMass() * cube2distance2Axis;
+            float I_cube2 = Cube2.GetTraegheitsmoment() + Cube2.GetMass() * cube2distance2Axis;
             Debug.Log("I_cube2: " + I_cube2);
             float I_total = I_cubeL + I_cube2;
             Debug.Log("I_total: " + I_total);
             // w = r_1 x m_1 * v_1 / I_total
-            float r_1 = Vector3.Distance(_cubeL.GetSchwerpunkt(), _cube2.transform.position);
+            float r_1 = Vector3.Distance(CubeL.GetSchwerpunkt(), Cube2.transform.position);
             Debug.Log("r_1: " + r_1);
-            float angleSpeed = r_1 * _cube2.GetMass() * _cube2.GetLastSpeed() / I_total;
+            float angleSpeed = r_1 * Cube2.GetMass() * Cube2.GetLastSpeed() / I_total;
             
             
             return angleSpeed;
